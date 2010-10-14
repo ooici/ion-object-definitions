fb0178998406ce3ce13e1ee532062c417341d2b6
Matt Rodriguez <mateo@Matt-Rodriguezs-MacBook-Pro.local>
2010-10-14 16:55:13 -0700

    Added distutils setup file to install generated files

---
+++ python/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup 
+import fnmatch
+import os
+
+pymods = []
+for root, dirnames, filenames in os.walk('net'):
+  for filename in fnmatch.filter(filenames, '*.py'):
+      if filename != "__init__.py": 
+          pymods.append(os.path.join(root, filename[:-3]))
+
+setup(name="ooi_data_format", version="0.000001", py_modules=pymods)
