746f98564361b7798119dc277a0c1e03fe02180c
Brian Fox <bfox@ucsd.edu>
2011-02-10 15:53:16 -0800

    Added topic to queue definition.  Bumped version.

--- python/setup.py
+++ python/setup.py
@@ -10,7 +10,7 @@
 
 setupdict = {
     'name' : 'ionproto',
-    'version' : '0.3.9',
+    'version' : '0.4.0',
     'description' : 'OOI LCA protocol buffers auto-generated code',
     'url': 'http://www.oceanobservatories.org/spaces/display/CIDev/LCAARCH+Development+Project',
     'download_url' : 'http://ooici.net/packages',
