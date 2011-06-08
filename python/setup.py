404f4f9d8f95201cf366eddb0a01156ca040d9e8
David Stuebe <dstuebe@asascience.com>
2011-06-08 16:51:11 -0700

    Release Version 0.3.26

--- python/setup.py
+++ python/setup.py
@@ -15,7 +15,7 @@ except ImportError:
 
 setup(
     name = 'ionproto',
-    version = '0.3.25',
+    version = '0.3.26',
     description = 'OOI LCA protocol buffers auto-generated code',
     url = 'http://www.oceanobservatories.org/spaces/display/CIDev/LCAARCH+Development+Project',
     download_url = 'http://ooici.net/releases',
