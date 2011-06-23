03c4afb997394e72c0d898121c4da6d170c3e6fb
Adam R. Smith <a2smith@ucsd.edu>
2011-06-23 08:04:57 -0700

    Release Version 0.3.30

--- python/setup.py
+++ python/setup.py
@@ -15,7 +15,7 @@ except ImportError:
 
 setup(
     name = 'ionproto',
-    version = '0.3.29',
+    version = '0.3.30',
     description = 'OOI LCA protocol buffers auto-generated code',
     url = 'http://www.oceanobservatories.org/spaces/display/CIDev/LCAARCH+Development+Project',
     download_url = 'http://ooici.net/releases',
